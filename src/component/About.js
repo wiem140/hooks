df19e814113e763a2c5101c82ad5271ae31ade3a
wiem140 <84313071+wiem140@users.noreply.github.com>
2021-08-03 17:02:32 +0100

    Add files via upload

---
+++ src/component/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <button> Back to home page</button>
+            <h1> About:</h1>
+            <h3> WorKshop</h3>
+        </div>
+
+    )
+}
+export default About
